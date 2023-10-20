18adbf52578a3ef48e460f2395d6850baac177b7
Igor <ig.zuban1ch@gmail.com>
2023-10-20 12:12:18 +0300

    feat: create user repository with necessary methods

---
+++ backend/src/main/java/org/linkwave/chat/repository/UserRepository.java
@@ -0,0 +1,17 @@
+package org.linkwave.chat.repository;
+
+import org.linkwave.chat.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+    Optional<UserEntity> findByUsername(String username);
+
+    @Query("select u from UserEntity u join fetch u.roles where u.id = :id")
+    Optional<UserEntity> findUserWithRoles(@Param("id") Long id);
+}
