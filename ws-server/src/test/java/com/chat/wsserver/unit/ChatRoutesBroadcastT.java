eefc22218d09d14f45864343de8466cc4f828db1
Igor <ig.zuban1ch@gmail.com>
2023-12-03 14:47:38 +0200

    feat: add unit tests for message broadcasting

---
+++ ws-server/src/test/java/com/chat/wsserver/unit/ChatRoutesBroadcastT.java
@@ -0,0 +1,29 @@
+package com.chat.wsserver.unit;
+
+import com.chat.wsserver.websocket.dto.Action;
+import com.chat.wsserver.websocket.dto.OutcomeMessage;
+import com.chat.wsserver.websocket.routing.bpp.Broadcast;
+import com.chat.wsserver.websocket.routing.bpp.SubRoute;
+import com.chat.wsserver.websocket.routing.bpp.WebSocketRoute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.socket.WebSocketSession;
+
+import java.time.ZonedDateTime;
+
+@WebSocketRoute("/group")
+public class ChatRoutesBroadcastT {
+
+    @SubRoute("/{id}/send")
+    @Broadcast("chat:{id}")
+    OutcomeMessage sendMessage(@PathVariable("id") long id,
+                               WebSocketSession session, String message) {
+        return OutcomeMessage.builder()
+                .action(Action.MESSAGE)
+                .chatId(id)
+                .sender(session.getId())
+                .text(message)
+                .timestamp(ZonedDateTime.now())
+                .build();
+    }
+
+}
