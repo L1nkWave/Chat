9d5d3b0ab2de159294bb786bb8de72cfe8e3849e
Igor <ig.zuban1ch@gmail.com>
2023-12-03 14:19:55 +0200

    feat: setup web socket server application

---
+++ ws-server/src/main/java/com/chat/wsserver/WebSocketServerApplication.java
@@ -0,0 +1,13 @@
+package com.chat.wsserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebSocketServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebSocketServerApplication.class, args);
+	}
+
+}
