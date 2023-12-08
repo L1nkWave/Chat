79cbb49a2b26b1e499bcd623b100f62ad5ca92da
Igor <ig.zuban1ch@gmail.com>
2023-12-08 14:23:27 +0200

    feat(ChatRepository): implement methods for find & clear user chats by session ID

--- ws-server/src/main/java/com/chat/wsserver/websocket/repository/ChatRepository.java
+++ ws-server/src/main/java/com/chat/wsserver/websocket/repository/ChatRepository.java
@@ -3,8 +3,10 @@ package com.chat.wsserver.websocket.repository;
 import java.util.Set;
 
 public interface ChatRepository {
-    void addMember(long chatId, String userId);
-    void removeMember(long chatId, String userId);
+    void addMember(long chatId, String sessionId);
+    void removeMember(long chatId, String sessionId);
     Set<String> getChatMembers(long chatId);
-    boolean isMember(long chatId, String userId);
+    boolean isMember(long chatId, String sessionId);
+    Set<Long> getUserChats(String sessionId);
+    boolean clearUserChats(String sessionId);
 }
--- ws-server/src/main/java/com/chat/wsserver/websocket/repository/RedisChatRepository.java
+++ ws-server/src/main/java/com/chat/wsserver/websocket/repository/RedisChatRepository.java
@@ -4,9 +4,12 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Repository;
 
+import java.util.Collections;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import static java.lang.Boolean.TRUE;
+import static java.lang.String.format;
 import static java.lang.String.valueOf;
 
 @Repository
@@ -16,18 +19,18 @@ public class RedisChatRepository implements ChatRepository {
     private final RedisTemplate<String, String> redisTemplate;
 
     @Override
-    public void addMember(long chatId, String userId) {
+    public void addMember(long chatId, String sessionId) {
         String chat = "chat:%d".formatted(chatId);
-        String userChats = "user:%s:chats".formatted(userId);
+        String userChats = "user:%s:chats".formatted(sessionId);
 
         redisTemplate.opsForSet().add(userChats, valueOf(chatId));
         redisTemplate.opsForSet().add(chat, userId);
     }
 
     @Override
-    public void removeMember(long chatId, String userId) {
+    public void removeMember(long chatId, String sessionId) {
         String chat = "chat:%d".formatted(chatId);
-        String userChats = "user:%s:chats".formatted(userId);
+        String userChats = "user:%s:chats".formatted(sessionId);
 
         redisTemplate.opsForSet().remove(userChats, valueOf(chatId));
         redisTemplate.opsForSet().remove(chat, userId);
@@ -40,9 +43,26 @@ public class RedisChatRepository implements ChatRepository {
     }
 
     @Override
-    public boolean isMember(long chatId, String userId) {
+    public boolean isMember(long chatId, String sessionId) {
         String chat = "chat:%d".formatted(chatId);
-        return TRUE.equals(redisTemplate.opsForSet().isMember(chat, userId));
+        return TRUE.equals(redisTemplate.opsForSet().isMember(chat, sessionId));
+    }
+
+    @Override
+    public Set<Long> getUserChats(String sessionId) {
+        String userChats = format("user:%s:chats", sessionId);
+        Set<String> members = redisTemplate.opsForSet().members(userChats);
+
+        return members == null ? Collections.emptySet() :
+                members.stream()
+                        .map(Long::parseLong)
+                        .collect(Collectors.toSet());
+    }
+
+    @Override
+    public boolean clearUserChats(String sessionId) {
+        String userChats = format("user:%s:chats", sessionId);
+        return redisTemplate.delete(userChats);
     }
 
 }
