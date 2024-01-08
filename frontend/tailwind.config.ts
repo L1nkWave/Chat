e07cc1e4840dedcb8db0766b243b75ee735a9edd
Artem Mahey <artemmagej@gmail.com>
2024-01-08 15:16:26 +0200

    feat: custom colors

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -7,6 +7,11 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    colors: {
+      "link-wave": {
+        midnight: "#121063",
+      },
+    },
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
