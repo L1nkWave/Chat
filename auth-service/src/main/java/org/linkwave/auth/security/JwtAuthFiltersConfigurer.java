f740b3f18a4712b31d74fe4fd96683e1edbe4900
Igor <ig.zuban1ch@gmail.com>
2024-01-16 19:49:40 +0200

    feat: provide security configuration

---
+++ auth-service/src/main/java/org/linkwave/auth/security/JwtAuthFiltersConfigurer.java
@@ -0,0 +1,80 @@
+package org.linkwave.auth.security;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.linkwave.auth.repository.DeactivatedTokenRepository;
+import org.linkwave.auth.security.filter.JwtLogoutFilter;
+import org.linkwave.auth.security.filter.JwtTokensInitializerFilter;
+import org.linkwave.auth.security.filter.JwtTokensRefreshFilter;
+import org.linkwave.auth.security.jwt.JwtAccessParser;
+import org.linkwave.auth.security.jwt.JwtAccessSerializer;
+import org.linkwave.auth.security.jwt.JwtRefreshParser;
+import org.linkwave.auth.security.jwt.JwtRefreshSerializer;
+import org.springframework.lang.NonNull;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.access.ExceptionTranslationFilter;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class JwtAuthFiltersConfigurer extends AbstractHttpConfigurer<JwtAuthFiltersConfigurer, HttpSecurity> {
+
+    private final ObjectMapper objectMapper;
+    private final DeactivatedTokenRepository tokenRepository;
+    private final UserDetailsService userDetailsService;
+
+    private JwtRefreshSerializer jwtRefreshSerializer;
+    private JwtAccessSerializer jwtAccessSerializer;
+    private JwtRefreshParser jwtRefreshParser;
+    private JwtAccessParser jwtAccessParser;
+
+    @Override
+    public void configure(@NonNull HttpSecurity builder) {
+
+        log.info("-> configure(): build filters");
+
+        // setup filters
+        final var jwtTokensInitializerFilter = new JwtTokensInitializerFilter(
+                objectMapper,
+                builder.getSharedObject(AuthenticationManager.class),
+                jwtRefreshSerializer, jwtAccessSerializer
+        );
+
+        final var jwtTokensRefreshFilter = new JwtTokensRefreshFilter(
+                jwtRefreshSerializer, jwtAccessSerializer, jwtRefreshParser,
+                tokenRepository, objectMapper, userDetailsService
+        );
+
+        final var jwtLogoutFilter = new JwtLogoutFilter(objectMapper, jwtAccessParser, tokenRepository);
+
+        // add filters to filter chain
+        builder.addFilterAfter(jwtTokensInitializerFilter, ExceptionTranslationFilter.class);
+        builder.addFilterAfter(jwtTokensRefreshFilter, ExceptionTranslationFilter.class);
+        builder.addFilterAfter(jwtLogoutFilter, ExceptionTranslationFilter.class);
+    }
+
+    public JwtAuthFiltersConfigurer setJwtRefreshSerializer(JwtRefreshSerializer jwtRefreshSerializer) {
+        this.jwtRefreshSerializer = jwtRefreshSerializer;
+        return this;
+    }
+
+    public JwtAuthFiltersConfigurer setJwtAccessSerializer(JwtAccessSerializer jwtAccessSerializer) {
+        this.jwtAccessSerializer = jwtAccessSerializer;
+        return this;
+    }
+
+    public JwtAuthFiltersConfigurer setJwtRefreshParser(JwtRefreshParser jwtRefreshParser) {
+        this.jwtRefreshParser = jwtRefreshParser;
+        return this;
+    }
+
+    public JwtAuthFiltersConfigurer setJwtAccessParser(JwtAccessParser jwtAccessParser) {
+        this.jwtAccessParser = jwtAccessParser;
+        return this;
+    }
+}
