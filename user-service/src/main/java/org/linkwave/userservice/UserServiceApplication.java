620af69e4f0706e4598782e6637a524e9dbb1049
Igor <ig.zuban1ch@gmail.com>
2024-01-16 20:12:14 +0200

    refactor: simply validate access token using public key

--- user-service/src/main/java/org/linkwave/userservice/UserServiceApplication.java
+++ user-service/src/main/java/org/linkwave/userservice/UserServiceApplication.java
@@ -1,13 +1,22 @@
 package org.linkwave.userservice;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UserServiceApplication.class, args);
+	}
+
+	@Bean
+	public ObjectMapper objectMapper() {
+		final var objectMapper = new ObjectMapper();
+		objectMapper.findAndRegisterModules();
+		return objectMapper;
 	}
 
 }
