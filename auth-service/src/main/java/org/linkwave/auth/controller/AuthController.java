9d16be8a553a4fb7f487b505c16562b7a4fa970a
Igor <ig.zuban1ch@gmail.com>
2024-01-16 19:52:03 +0200

    feat: add end-point for token validation

---
+++ auth-service/src/main/java/org/linkwave/auth/controller/AuthController.java
@@ -0,0 +1,51 @@
+package org.linkwave.auth.controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.linkwave.auth.dto.ApiError;
+import org.linkwave.auth.repository.DeactivatedTokenRepository;
+import org.linkwave.auth.security.jwt.JwtAccessParser;
+import org.springframework.http.ResponseEntity;
+import org.springframework.lang.NonNull;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.Instant;
+
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+import static org.springframework.http.ResponseEntity.badRequest;
+import static org.springframework.http.ResponseEntity.noContent;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final JwtAccessParser accessParser;
+    private final DeactivatedTokenRepository tokenRepository;
+
+    @GetMapping("/validate-token")
+    public ResponseEntity<ApiError> validateToken(@NonNull HttpServletRequest request) {
+
+        final var authHeader = request.getHeader(AUTHORIZATION);
+        final var error = ApiError.builder()
+                .path(request.getRequestURI())
+                .statusCode(BAD_REQUEST.value())
+                .timestamp(Instant.now());
+
+        if (authHeader != null && authHeader.startsWith("Bearer ")) {
+
+            final var token = accessParser.parse(authHeader.substring(7));
+            if (token == null || tokenRepository.existsById(token.id())) {
+                return badRequest().body(error.message("Invalid access token").build());
+            }
+
+            return noContent().build();
+        }
+
+        return badRequest().body(error.message("Bearer is not present").build());
+    }
+
+}
