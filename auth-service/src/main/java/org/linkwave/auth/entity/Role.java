a123801a3d1c4e3f9ad33b812306d5b63d34e0ea
Igor <ig.zuban1ch@gmail.com>
2024-01-19 19:28:57 +0200

    feat: update entities

--- auth-service/src/main/java/org/linkwave/auth/entity/Role.java
+++ auth-service/src/main/java/org/linkwave/auth/entity/Role.java
@@ -4,14 +4,19 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.util.LinkedList;
 import java.util.List;
 
 @Entity
 @Table(name = "roles")
+@NoArgsConstructor
+@AllArgsConstructor
 @Getter
+@Builder
 public class Role {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
     @Column(unique = true, nullable = false)
@@ -19,6 +24,18 @@ public class Role {
 
     @JsonIgnore
     @ManyToMany(mappedBy = "roles")
-    private List<User> users;
+    @Builder.Default
+    private List<User> users = new LinkedList<>();
+
+    @Getter
+    @RequiredArgsConstructor
+    public enum Roles {
+
+        USER("ROLE_USER"),
+        ADMIN("ROLE_ADMIN");
+
+        private final String name;
+
+    }
 
 }
--- auth-service/src/main/java/org/linkwave/auth/entity/User.java
+++ auth-service/src/main/java/org/linkwave/auth/entity/User.java
@@ -1,19 +1,29 @@
 package org.linkwave.auth.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+import java.util.LinkedList;
 import java.util.List;
 
 @Entity
 @Table(name = "users")
+@NoArgsConstructor
+@AllArgsConstructor
 @Getter
+@Builder
 public class User {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(unique = true)
     private String username;
+
     private String password;
 
     @ManyToMany
@@ -22,6 +32,12 @@ public class User {
             joinColumns = @JoinColumn(name = "user_id"),
             inverseJoinColumns = @JoinColumn(name = "role_id")
     )
-    private List<Role> roles;
+    @Builder.Default
+    private List<Role> roles = new LinkedList<>();
+
+    public void addRole(Role role) {
+        this.roles.add(role);
+        role.getUsers().add(this);
+    }
 
 }
