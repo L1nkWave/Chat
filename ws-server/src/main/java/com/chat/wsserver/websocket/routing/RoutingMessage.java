3fee7b562ef223e48a1327252ae0b2c67971cf4c
Igor <ig.zuban1ch@gmail.com>
2024-01-23 15:45:07 +0200

    feat: add documentation to architecture components

--- ws-server/src/main/java/com/chat/wsserver/websocket/routing/RoutingMessage.java
+++ ws-server/src/main/java/com/chat/wsserver/websocket/routing/RoutingMessage.java
@@ -1,4 +1,8 @@
 package com.chat.wsserver.websocket.routing;
 
+/**
+ * @param path    that message needs to reach to be handled
+ * @param payload raw payload of received message
+ */
 public record RoutingMessage(String path, String payload) {
 }
