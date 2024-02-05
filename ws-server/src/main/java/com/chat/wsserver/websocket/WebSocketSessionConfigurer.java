c12f509f6534cf08373550266f18374dc2b476ac
Igor <ig.zuban1ch@gmail.com>
2024-02-05 12:58:17 +0200

    refactor(WebSocketSessionConfigurer): rename configuration method

--- ws-server/src/main/java/com/chat/wsserver/websocket/WebSocketSessionConfigurer.java
+++ ws-server/src/main/java/com/chat/wsserver/websocket/WebSocketSessionConfigurer.java
@@ -4,5 +4,5 @@ import org.springframework.lang.NonNull;
 import org.springframework.web.socket.WebSocketSession;
 
 public interface WebSocketSessionConfigurer {
-    WebSocketSession getConcurrentSession(@NonNull WebSocketSession session);
+    WebSocketSession configure(@NonNull WebSocketSession session);
 }
