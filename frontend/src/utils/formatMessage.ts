eaa362a1f2ca86641ead00706176b476f26f1cbd
Artem Magei <artemmagej@gmail.com>
2024-02-05 21:17:38 +0200

    feat: format message util

---
+++ frontend/src/utils/formatMessage.ts
@@ -0,0 +1,11 @@
+export const formatMessage = (
+  input: string | undefined
+): string | undefined => {
+  if (!input) {
+    return input;
+  }
+  const capitalizedString = input.charAt(0).toUpperCase() + input.slice(1);
+  return /[.!]$/.test(capitalizedString)
+    ? capitalizedString
+    : `${capitalizedString}.`;
+};
