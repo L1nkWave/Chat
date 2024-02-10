0c603f9178b59af242eb8e3bca81215a311ae231
Artem Magei <artemmagej@gmail.com>
2024-02-10 14:18:37 +0200

    feat: create socket api

---
+++ frontend/src/api/socket/index.ts
@@ -0,0 +1,25 @@
+import { ChatType } from "@/api/socket/index.types";
+
+const socketUrl =
+  process.env.NEXT_PUBLIC_WEB_SOCKET_URL ?? "ws://localhost:8080";
+
+export const connectToSocket = (token: string) => {
+  return new WebSocket(`${socketUrl}?access=${token}`);
+};
+
+export const sendChatMessage = (
+  socket: WebSocket,
+  message: string,
+  chatType: ChatType,
+  chatId: number | string
+) => {
+  socket.send(`path=/${chatType}/${chatId}/send
+  
+  
+    ${message}
+  `);
+};
+
+export const disconnectFromSocket = (socket: WebSocket) => {
+  socket.close();
+};
