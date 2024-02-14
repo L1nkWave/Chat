b846fa3554b03dc660fee58608da1b68846f21e4
Artem Magei <artemmagej@gmail.com>
2024-02-14 16:19:15 +0200

    feat: add chat component tp page

--- frontend/src/app/chat/page.tsx
+++ frontend/src/app/chat/page.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 
+import { Chat } from "@/components/Chat/Chat";
 import { Container } from "@/components/Container/Container";
 import { SocketProvider } from "@/context/SocketProvider/SocketProvider";
 
 export default function ChatPage() {
   return (
     <Container showHeader={false}>
-      <SocketProvider>Chat</SocketProvider>
+      <SocketProvider>
+        <Chat />
+      </SocketProvider>
     </Container>
   );
 }
