83557333c6809a19ee37cf68bc6e53982aab5277
Artem Magei <artemmagej@gmail.com>
2024-02-14 14:29:13 +0200

    feat: add tailwind scrollbar

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -18,7 +18,7 @@ const config: Config = {
         "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
     },
   },
-  plugins: [],
+  plugins: [require("tailwind-scrollbar")],
   darkMode: "class",
 };
 
