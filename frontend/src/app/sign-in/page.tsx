91be540dbb335dd4153e2dc1ba0082d4054926e2
Artem Magei <artemmagej@gmail.com>
2024-03-14 16:52:40 +0200

    refactor: rename components on auth pages

--- frontend/src/app/sign-in/page.tsx
+++ frontend/src/app/sign-in/page.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { SignInForm } from "@/components/AuthForms/SignInForm/SignInForm";
+import { SignInForm } from "@/components/Auth/SignInForm/SignInForm";
 import { Container } from "@/components/Container/Container";
 
 export default function SignIn() {
