676aa787c30c9ced0f981f28416476000763b80d
Artem Magei <artemmagej@gmail.com>
2024-03-15 17:56:18 +0200

    feat: chat

--- frontend/src/components/Chat/Chat.tsx
+++ frontend/src/components/Chat/Chat.tsx
@@ -2,19 +2,54 @@
 
 import Image from "next/image";
 import { useRouter } from "next/navigation";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
-import { ChatList } from "@/components/Chat/ChatList/ChatList";
+import { getContacts } from "@/api/http/users/users";
+import { ContactParams } from "@/api/http/users/users.types";
+import { SIDEBAR_ICON_SIZE } from "@/components/Chat/chat.config";
+import { InteractiveList } from "@/components/Chat/InteractiveList/InteractiveList";
+import { MainBox } from "@/components/Chat/MainBox/MainBox";
 import { SideBar } from "@/components/Chat/SideBar/SideBar";
-import { useAppSelector } from "@/redux/hooks";
-import { decodeToken } from "@/utils/DecodeToken/decodeToken";
+import { CustomButton } from "@/components/CustomButton/CustomButton";
+import { decodeToken } from "@/helpers/DecodeToken/decodeToken";
+import { setCurrentInteractiveList } from "@/lib/features/chat/chatSlice";
+import { useAppDispatch, useAppSelector } from "@/lib/hooks";
 
 export function Chat() {
+  const dispatch = useAppDispatch();
+
   const { accessToken } = useAppSelector(state => state.auth);
   const { webSocket } = useAppSelector(state => state.socket);
+  const { currentMainBox, currentInteractiveList } = useAppSelector(state => state.chat);
+
   const router = useRouter();
 
+  const [contacts, setContacts] = useState<ContactParams[]>([]);
+
   useEffect(() => {
+    const fetchContacts = async () => {
+      try {
+        let fetchedContacts = await getContacts();
+        fetchedContacts = fetchedContacts.map(contact => {
+          if (contact.user.id === 2)
+            return {
+              ...contact,
+              user: {
+                ...contact.user,
+                online: true,
+              },
+            };
+          return contact;
+        });
+
+        setContacts(fetchedContacts);
+        console.log(fetchedContacts);
+      } catch (error) {
+        console.error("Error fetching contacts:", error);
+      }
+    };
+
+    fetchContacts();
     if (!accessToken) {
       return router.push("/sign-in");
     }
@@ -46,6 +81,15 @@ export function Chat() {
       webSocket.removeEventListener("close", () => {});
     };
   }, [accessToken, router, webSocket]);
+
+  const handleContactsClick = () => {
+    dispatch(setCurrentInteractiveList("contacts"));
+  };
+
+  const handleChatsClick = () => {
+    dispatch(setCurrentInteractiveList("chats"));
+  };
+
   if (webSocket?.readyState === WebSocket.CLOSED) {
     return (
       <div className="w-full h-full">
@@ -62,11 +106,23 @@ export function Chat() {
 
   return (
     <div className="w-screen flex px-64">
-      <SideBar />
-      <ChatList />
-      <div className="flex w-full bg-dark-550 p-2 h-screen items-center justify-center rounded-r-2xl text-gray-300">
-        Choose chat room to start chatting
-      </div>
+      <SideBar>
+        <CustomButton
+          variant="square"
+          icon="chat-plus-outline"
+          iconSize={SIDEBAR_ICON_SIZE}
+          onClick={handleChatsClick}
+        />
+        <CustomButton
+          variant="square"
+          icon="group-outline"
+          iconSize={SIDEBAR_ICON_SIZE}
+          onClick={handleContactsClick}
+        />
+        <CustomButton variant="square" iconSize={SIDEBAR_ICON_SIZE} icon="setting-outline" />
+      </SideBar>
+      <InteractiveList interactiveListVariant={currentInteractiveList} contacts={contacts} chats={[]} />
+      <MainBox mainBoxVariant={currentMainBox} />
     </div>
   );
 }
