1fc5e84161d691f645d7b6640e36c7e96b1dbcfb
Artem Magei <artemmagej@gmail.com>
2024-03-15 17:52:59 +0200

    feat: make sidebar like container

--- frontend/src/components/Chat/SideBar/SideBar.tsx
+++ frontend/src/components/Chat/SideBar/SideBar.tsx
@@ -1,31 +1,9 @@
-import {
-  ChatPlusOutlineIcon,
-  GroupOutlineIcon,
-  SettingOutlineIcon,
-} from "@public/icons";
-import React from "react";
+import React, { PropsWithChildren } from "react";
 
-import { ICON_SIZE } from "@/components/Chat/SideBar/sideBar.config";
-import { CustomButton } from "@/components/CustomButton/CustomButton";
-
-export function SideBar() {
+export function SideBar({ children }: Readonly<PropsWithChildren>) {
   return (
     <div className="flex flex-col w-1/8 bg-dark-250 p-4 h-screen rounded-l-2xl gap-4 text-blue-500 text-lg">
-      <CustomButton
-        variant="square"
-        icon={<ChatPlusOutlineIcon />}
-        iconSize={ICON_SIZE}
-      />
-      <CustomButton
-        variant="square"
-        icon={<GroupOutlineIcon />}
-        iconSize={ICON_SIZE}
-      />
-      <CustomButton
-        variant="square"
-        icon={<SettingOutlineIcon />}
-        iconSize={ICON_SIZE}
-      />
+      {children}
     </div>
   );
 }
