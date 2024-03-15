0a5eb8d33e963832e9102b8f9c9db53496f8a8bb
Artem Magei <artemmagej@gmail.com>
2024-03-15 17:55:22 +0200

    feat: main box

---
+++ frontend/src/components/Chat/MainBox/MainBox.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import { MainBoxProps } from "@/components/Chat/MainBox/mainBox.types";
+
+export function MainBox({ mainBoxVariant }: Readonly<MainBoxProps>) {
+  if (mainBoxVariant) {
+    return null;
+  }
+  return (
+    <div className="flex w-full bg-dark-550 p-2 h-screen items-center justify-center rounded-r-2xl text-gray-300">
+      Choose chat room to start chatting
+    </div>
+  );
+}
