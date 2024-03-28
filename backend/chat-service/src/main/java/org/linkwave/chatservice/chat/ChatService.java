185504a0c768dc666b719b5c9a5a96d1b16f6edd
Igor <ig.zuban1ch@gmail.com>
2024-03-29 01:04:19 +0200

    feat: add read messages endpoint

--- backend/chat-service/src/main/java/org/linkwave/chatservice/chat/ChatService.java
+++ backend/chat-service/src/main/java/org/linkwave/chatservice/chat/ChatService.java
@@ -14,6 +14,7 @@ import org.springframework.lang.NonNull;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 public interface ChatService {
