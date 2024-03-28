ab80451c316b9884e920d285561140438a3f0614
Igor <ig.zuban1ch@gmail.com>
2024-03-28 14:44:27 +0200

    feat: implement user service

---
+++ backend/chat-service/src/main/java/org/linkwave/chatservice/message/ChatMessageCursor.java
@@ -0,0 +1,17 @@
+package org.linkwave.chatservice.message;
+
+import lombok.*;
+
+import java.time.Instant;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
+public class ChatMessageCursor {
+
+    private String chatId;
+    private String messageId;
+    private Instant messageCreation;
+
+}
---
+++ backend/chat-service/src/main/java/org/linkwave/chatservice/user/User.java
@@ -0,0 +1,34 @@
+package org.linkwave.chatservice.user;
+
+import lombok.*;
+import org.linkwave.chatservice.message.ChatMessageCursor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Document("users")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode(of = {"id", "userId"})
+@Builder
+public class User {
+
+    @Id
+    private String id;
+
+    @Indexed(unique = true)
+    private Long userId;
+
+    @Builder.Default
+    private List<ChatMessageCursor> chatMessageCursors = new ArrayList<>();
+
+    public void addChatMessageCursor(ChatMessageCursor cursor) {
+        chatMessageCursors.add(cursor);
+    }
+
+}
