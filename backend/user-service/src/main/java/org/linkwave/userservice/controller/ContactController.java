cb794567b15bdea7263d1f6161c1f761d78ef48b
Igor <ig.zuban1ch@gmail.com>
2024-04-01 20:48:50 +0300

    feat: add and remove contact

--- backend/user-service/src/main/java/org/linkwave/userservice/controller/ContactController.java
+++ backend/user-service/src/main/java/org/linkwave/userservice/controller/ContactController.java
@@ -1,17 +1,17 @@
 package org.linkwave.userservice.controller;
 
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.linkwave.shared.auth.DefaultUserDetails;
 import org.linkwave.userservice.dto.ContactDto;
+import org.linkwave.userservice.dto.NewContactRequest;
 import org.linkwave.userservice.service.ContactService;
 import org.springframework.data.util.Pair;
+import org.springframework.http.HttpStatus;
 import org.springframework.lang.NonNull;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -31,6 +31,18 @@ public class ContactController {
         final Pair<Integer, List<ContactDto>> result = contactService.getContactsByUsername(getDetails().id(), username, offset, limit);
         response.setHeader(TOTAL_COUNT.getValue(), String.valueOf(result.getFirst()));
         return result.getSecond();
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void addContact(@Valid @RequestBody NewContactRequest newContact) {
+        contactService.addContact(getDetails().id(), newContact);
+    }
+
+    @DeleteMapping("/{contactId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void removeContact(@PathVariable Long contactId) {
+        contactService.removeContact(getDetails().id(), contactId);
     }
 
     private DefaultUserDetails getDetails() {
--- backend/user-service/src/main/java/org/linkwave/userservice/service/ContactService.java
+++ backend/user-service/src/main/java/org/linkwave/userservice/service/ContactService.java
@@ -1,7 +1,9 @@
 package org.linkwave.userservice.service;
 
 import org.linkwave.userservice.dto.ContactDto;
+import org.linkwave.userservice.dto.NewContactRequest;
 import org.springframework.data.util.Pair;
+import org.springframework.lang.NonNull;
 
 import java.util.List;
 
@@ -16,5 +18,21 @@ public interface ContactService {
      * and the second - selected contacts with offset and limit params
      */
     Pair<Integer, List<ContactDto>> getContactsByUsername(Long userId, String username, int offset, int limit);
+
+    /**
+     * Adds new contact to initiator's contacts list.
+     *
+     * @param initiatorId id of the request initiator
+     * @param newContact new contact information
+     */
+    void addContact(@NonNull Long initiatorId, @NonNull NewContactRequest newContact);
+
+    /**
+     * Removes specified contact from initiator's contacts list.
+     *
+     * @param initiatorId id of the request initiator
+     * @param contactId id of the contact to remove
+     */
+    void removeContact(@NonNull Long initiatorId, Long contactId);
 
 }
