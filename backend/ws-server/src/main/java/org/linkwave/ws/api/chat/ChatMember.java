337466afcd0cf0ed302c10304dd53f65b75a3ae1
Igor <ig.zuban1ch@gmail.com>
2024-04-08 13:22:22 +0300

    feat: mock join group chat request

--- backend/ws-server/src/main/java/org/linkwave/ws/api/chat/ChatMember.java
+++ backend/ws-server/src/main/java/org/linkwave/ws/api/chat/ChatMember.java
@@ -1,12 +1,16 @@
 package org.linkwave.ws.api.chat;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.time.Instant;
 
 @NoArgsConstructor
+@AllArgsConstructor
 @Getter
+@Builder
 public class ChatMember {
 
     private Long id;
