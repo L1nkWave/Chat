904bd80820df25cd1feae579c97f1b5e93d34bbb
Artem Magei <artemmagej@gmail.com>
2024-04-17 00:25:35 +0300

    feat: created user info box

--- frontend/src/components/Chat/MainBox/variants/UserInfoBox/UserInfoBox.tsx
+++ frontend/src/components/Chat/MainBox/variants/UserInfoBox/UserInfoBox.tsx
@@ -1,43 +1,41 @@
 import { Avatar } from "@/components/Avatar/Avatar";
 import { InfoTextBox } from "@/components/Chat/MainBox/variants/UserInfoBox/InfoBox/InfoTextBox";
 import { InfoIconShape } from "@/components/Chat/MainBox/variants/UserInfoBox/InfoIconShape/InfoIconShape";
+import { UserInfoBoxProps } from "@/components/Chat/MainBox/variants/UserInfoBox/userInfoBox.types";
 import { CustomButton } from "@/components/CustomButton/CustomButton";
 import { Icon } from "@/components/Icon/Icon";
 import { LastSeen } from "@/components/LastSeen/LastSeen";
+import { getContactName } from "@/helpers/contactHelpers";
 
-export function UserInfoBox() {
+export function UserInfoBox({ contact }: Readonly<UserInfoBoxProps>) {
   return (
     <div className="m-6 flex flex-col w-full h-fit border-2 rounded-2xl border-dark-100 bg-dark-350">
       <div className="p-16">
         <div className="flex">
-          <Avatar
-            className="w-32 h-32"
-            item={{
-              id: 1,
-            }}
-            alt=""
-          />
+          <Avatar className="w-32 h-32" item={contact.user} alt="Avatar" />
           <div className="px-10 flex flex-col justify-center">
-            <h1 className="text-3xl text-gray-200">Daniel Bard</h1>
+            <h1 className="text-3xl text-gray-200">{getContactName(contact)}</h1>
             <LastSeen
+              online={contact.user.online}
+              lastSeen={contact.user.lastSeen}
               iconSize={24}
               className="font-semibold"
               textColor="text-blue-300"
-              online={false}
-              lastSeen={Date.now() - 100000000}
             />
           </div>
         </div>
+
         <div className="py-8">
           <InfoTextBox>
             <InfoIconShape icon="link-outline" />
-            @jam3leub9
+            {contact.user.username}
           </InfoTextBox>
           <InfoTextBox>
             <InfoIconShape icon="list-outline" />
-            Boi vooeoroeddddddddddddddddddddddddddddddddddddddd ddddddddddddddddddddddddddddddddddddddddddddddddddd
+            {contact.user.bio}
           </InfoTextBox>
         </div>
+
         <div className="flex justify-center gap-10 pt-10">
           <CustomButton variant="outline" type="button" className="text-blue-200">
             <Icon name="left-angle" iconSize={32} />
---
+++ frontend/src/components/Chat/MainBox/variants/UserInfoBox/userInfoBox.types.ts
@@ -0,0 +1,5 @@
+import { ContactParams } from "@/api/http/users/users.types";
+
+export type UserInfoBoxProps = {
+  contact: ContactParams;
+};
