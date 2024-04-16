5b8a0f0434a4f4283701050a47eba124fbd448fe
Artem Magei <artemmagej@gmail.com>
2024-04-17 00:23:58 +0300

    feat: remove socket slice from redux

--- frontend/src/lib/features/socket/socketSlice.config.ts
+++
@@ -1,6 +0,0 @@
-import { SocketStateParams } from "@/lib/features/socket/socketSlice.types";
-
-export const SOCKET_SLICE_NAME = "socket";
-export const socketInitialState: SocketStateParams = {
-  webSocket: null,
-};
--- frontend/src/lib/features/socket/socketSlice.types.ts
+++
@@ -1,3 +0,0 @@
-export type SocketStateParams = {
-  webSocket: WebSocket | null;
-};
