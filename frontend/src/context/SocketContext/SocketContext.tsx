e874459b43b7562ed051d6f8bc2960fdf466f39b
Artem Magei <artemmagej@gmail.com>
2024-04-28 10:50:01 +0300

    feat: update sidebar

--- frontend/src/context/SocketContext/SocketContext.tsx
+++ frontend/src/context/SocketContext/SocketContext.tsx
@@ -2,6 +2,6 @@ import { createContext } from "react";
 
 import { SocketContextProps } from "@/context/SocketContext/socketContext.types";
 
-const SocketContext = createContext<SocketContextProps>({ webSocket: null });
+const SocketContext = createContext<SocketContextProps>({});
 
 export { SocketContext };
