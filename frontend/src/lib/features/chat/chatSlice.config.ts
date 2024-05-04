57bff87a0b8100a545a0033c98d92752825bab3f
Artem Magei <artemmagej@gmail.com>
2024-05-04 20:09:48 +0300

    refactor: naming

--- frontend/src/lib/features/chat/chatSlice.config.ts
+++ frontend/src/lib/features/chat/chatSlice.config.ts
@@ -2,6 +2,6 @@ import { ChatStateParams } from "@/lib/features/chat/chatSlice.types";
 
 export const CHAT_SLICE_NAME = "chat";
 export const chatInitialState: ChatStateParams = {
-  currentMainBox: "empty",
-  currentInteractiveList: "chats",
+  currentMainBoxState: "empty",
+  currentInteractiveListState: "chats",
 };
