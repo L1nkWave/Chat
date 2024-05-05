718e992d09567f577ee550637691e01fb37c272c
Artem Magei <artemmagej@gmail.com>
2024-05-05 16:49:26 +0300

    feat: update api and mapping responses

--- frontend/src/api/http/chat/chat.ts
+++ frontend/src/api/http/chat/chat.ts
@@ -1,16 +1,15 @@
 import { instance } from "@/api/http";
-import { ChatParams, UserParams } from "@/api/http/contacts/contacts.types";
+import { ChatParams } from "@/api/http/contacts/contacts.types";
 
 export async function addDuoChat(userId: string) {
   const body = {
     recipient: userId,
   };
-  const { data } = await instance.post<UserParams>(`chats`, body);
+  const { data } = await instance.post(`chats`, body);
   return data;
 }
 
 export async function getChats(offset: number = 0, limit: number = 10) {
   const { data } = await instance.get<ChatParams[]>(`chats?limit=${limit}&offset=${offset}`);
-  console.log(data);
   return data;
 }
--- frontend/src/api/http/contacts/contacts.ts
+++ frontend/src/api/http/contacts/contacts.ts
@@ -1,15 +1,23 @@
 import { instance } from "@/api/http";
-import { UserParams } from "@/api/http/contacts/contacts.types";
-import { Contacts } from "@/components/Chat/InteractiveList/interactiveList.types";
+import { ContactParams, UserParams } from "@/api/http/contacts/contacts.types";
 
 export async function getContacts() {
-  const { data } = await instance.get<Contacts>("users/contacts?username=&limit=10&offset=0");
-  return data;
+  const { data } = await instance.get<ContactParams[]>("users/contacts?username=&limit=10&offset=0");
+  const contacts = new Map<number, ContactParams>();
+  data.forEach(contact => {
+    contacts.set(contact.user.id, contact);
+  });
+
+  return contacts;
 }
 
 export async function searchContacts(username: string = "", limit: number = 10, offset: number = 0) {
   const { data } = await instance.get<UserParams[]>(`users?username=${username}&limit=${limit}&offset=${offset}`);
-  return data;
+  const users = new Map<number, UserParams>();
+  data.forEach(user => {
+    users.set(user.id, user);
+  });
+  return users;
 }
 
 export async function addContact(userId: string, alias: string) {
