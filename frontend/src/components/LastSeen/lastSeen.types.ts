d04d36087841f4f037d6857173e7d9b1354adb89
Artem Magei <artemmagej@gmail.com>
2024-05-11 20:18:58 +0300

    feat: update last seen

--- frontend/src/components/LastSeen/lastSeen.types.ts
+++ frontend/src/components/LastSeen/lastSeen.types.ts
@@ -2,7 +2,7 @@ export type TextColors = "text-blue-300" | "text-gray-400";
 
 export type LastSeenProps = {
   online: boolean;
-  lastSeen?: number;
+  lastSeen: number;
   className?: string;
   iconSize?: number;
   textColor?: TextColors;
