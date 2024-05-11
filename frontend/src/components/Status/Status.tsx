7634946e5933f50d9c0563ac2278a1fe84cc0dac
Artem Magei <artemmagej@gmail.com>
2024-05-11 20:15:40 +0300

    feat: update status component

--- frontend/src/components/Status/Status.tsx
+++ frontend/src/components/Status/Status.tsx
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 import { StatusProps } from "@/components/Status/status.types";
+import { UserStatus } from "@/lib/features/user/userSlice.types";
 
-export function Status({ className, ...props }: StatusProps) {
-  return <div className={`bg-green rounded-full w-2.5 h-2.5 mr-1 ${className}`} {...props} />;
+export function Status({ className, textStatus, online, classNameTextContainer, ...props }: Readonly<StatusProps>) {
+  useEffect(() => {
+    console.log("Status component rendered!", online, textStatus);
+  }, []);
+
+  if (textStatus) {
+    return (
+      <div className={`flex items-center text-gray-300 ${!!classNameTextContainer && classNameTextContainer}`}>
+        <Status online />
+        {online ? UserStatus.ONLINE : UserStatus.OFFLINE}
+      </div>
+    );
+  }
+
+  return online && <div className={`bg-green rounded-full w-2.5 h-2.5 mr-1 ${!!className && className}`} {...props} />;
 }
--- frontend/src/components/Status/status.types.ts
+++ frontend/src/components/Status/status.types.ts
@@ -1,3 +1,7 @@
 import { DetailedHTMLProps, HTMLAttributes } from "react";
 
-export type StatusProps = DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement>;
+export type StatusProps = {
+  textStatus?: boolean;
+  classNameTextContainer?: string;
+  online: boolean;
+} & DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement>;
