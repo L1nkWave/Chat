0d6449d4f8610c03c0793f3b2ac99a7ce99be3b2
Artem Magei <artemmagej@gmail.com>
2024-05-23 04:38:48 +0300

    feat: update user items

--- frontend/src/components/Chat/UserItem/variants/ChatItem.tsx
+++ frontend/src/components/Chat/UserItem/variants/ChatItem.tsx
@@ -1,3 +1,6 @@
+import { format } from "date-fns";
+import React from "react";
+
 import { Avatar } from "@/components/Avatar/Avatar";
 import { ChatItemProps } from "@/components/Chat/UserItem/userItem.types";
 
@@ -14,21 +17,22 @@ export function ChatItem({ chat, onClick, className }: Readonly<ChatItemProps>) 
     <button
       onClick={handleClick}
       type="button"
-      className={`flex flex-row justify-between items-center outline-none bg-dark-300 p-4 rounded-lg hover:bg-dark-200 ${className}`}
+      className={`w-full relative flex flex-row justify-center items-center outline-none bg-dark-300 p-4 rounded-lg hover:bg-dark-200 ${className ?? ""}`}
     >
-      <div className="flex flex-row">
-        <div className="w-14 h-14">
-          <Avatar item={{ id: 1 }} alt="User Avatar" />
+      <div className="flex flex-row w-10/12 mr-4">
+        <div className="min-w-max">
+          <Avatar item={chat.user} alt="User Avatar" online={chat.user.online} />
         </div>
-        <div className="flex flex-col ml-4 justify-center items-start">
-          <span className="text-lg">{chat.lastMessage.author.name}</span>
-          <p className="text-gray-300"> {chat.lastMessage.text} </p>
+        <div className="flex relative w-4/6 flex-col ml-4 justify-center items-start">
+          <span className="text-lg inline w-full text-start truncate">{chat.user.name}</span>
+          <span className="text-gray-300 truncate w-full text-start">{chat.lastMessage?.text}</span>
         </div>
       </div>
-      <div>
-        <p className="text-gray-300 text-sm">16:50</p>
-        <div className="flex justify-center items-center bg-blue-300 rounded-lg mt-1">
-          {!!chat.createdAt && <p className="text-white text-xs p-1">{1}</p>}
+
+      <div className="min-w-10">
+        <p className="text-gray-300 text-sm">{format(chat.createdAt, "HH:mm")}</p>
+        <div className="flex w-full justify-center items-center bg-blue-300 rounded-lg mt-1">
+          {!!chat.unreadMessages && <p className="text-white text-xs p-1">{chat.unreadMessages}</p>}
         </div>
       </div>
     </button>
