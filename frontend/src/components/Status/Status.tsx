29b32c44fa2d2f0cf7970b2633891368d4a60968
Artem Magei <artemmagej@gmail.com>
2024-05-23 04:34:27 +0300

    feat: update status component

--- frontend/src/components/Status/Status.tsx
+++ frontend/src/components/Status/Status.tsx
@@ -1,13 +1,9 @@
-import React, { useEffect } from "react";
+import React from "react";
 
 import { StatusProps } from "@/components/Status/status.types";
 import { UserStatus } from "@/lib/features/user/userSlice.types";
 
 export function Status({ className, textStatus, online, classNameTextContainer, ...props }: Readonly<StatusProps>) {
-  useEffect(() => {
-    console.log("Status component rendered!", online, textStatus);
-  }, []);
-
   if (textStatus) {
     return (
       <div className={`flex items-center text-gray-300 ${!!classNameTextContainer && classNameTextContainer}`}>
@@ -17,5 +13,9 @@ export function Status({ className, textStatus, online, classNameTextContainer, 
     );
   }
 
-  return online && <div className={`bg-green rounded-full w-2.5 h-2.5 mr-1 ${!!className && className}`} {...props} />;
+  return (
+    online && (
+      <div className={`bg-green rounded-full w-2.5 h-2.5 mr-1 ${!!className && className}`} {...props} />
+    )
+  );
 }
