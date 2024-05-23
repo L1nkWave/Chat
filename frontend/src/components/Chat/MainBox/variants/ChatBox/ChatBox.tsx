53a3af2b13ff5264bed25bc4ed3b5b15a34626e1
Artem Magei <artemmagej@gmail.com>
2024-05-23 04:37:59 +0300

    feat: update messages and chat components

--- frontend/src/components/Chat/MainBox/variants/ChatBox/ChatBox.tsx
+++ frontend/src/components/Chat/MainBox/variants/ChatBox/ChatBox.tsx
@@ -1,169 +1,133 @@
-import React from "react";
+import React, { useLayoutEffect, useRef, useState } from "react";
 
-import { MessageParams } from "@/api/http/contacts/contacts.types";
+import { ContactParams } from "@/api/http/contacts/contacts.types";
+import { MESSAGE_CONTAINER, MESSAGE_INPUT } from "@/components/Chat/MainBox/variants/ChatBox/chatBox.config";
 import { ChatBoxProps } from "@/components/Chat/MainBox/variants/ChatBox/chatBox.types";
 import { ChatHeader } from "@/components/Chat/MainBox/variants/ChatBox/ChatHeader/ChatHeader";
 import { MessageContainer } from "@/components/Chat/MainBox/variants/ChatBox/MessageBox/MessageContainer/MessageContainer";
 import { MessageInput } from "@/components/Chat/MainBox/variants/ChatBox/MessageInput/MessageInput";
+import { useAppSelector } from "@/lib/hooks";
 
-export function ChatBox({ contact }: Readonly<ChatBoxProps>) {
-  const messages: MessageParams[] = [
-    {
-      id: "1",
-      action: "message",
-      reactions: [],
-      createdAt: "2412532412412",
-      edited: false,
-      isRead: true,
-      text: "We want to encourage a robust community for the Spring open source project.",
-      author: { id: 1, online: true, username: "bot", name: "Bot", lastSeen: 0, createdAt: "", avatarPath: "" },
-    },
-    {
-      id: "2",
-      action: "message",
-      reactions: [],
-      createdAt: "",
-      edited: false,
-      isRead: true,
-      text: "Therefore, you may do any of the following, as long as you do so in a way that does not devalue. In other words, when you do these things, you should behave responsibly and reasonably in the interest of the community, but you do not need a trademark license from us to do them.",
-      author: { id: 1, online: true, username: "bot", name: "Bot", lastSeen: 0, createdAt: "", avatarPath: "" },
-    },
-    {
-      id: "3",
-      action: "message",
-      reactions: [],
-      createdAt: "124125125324",
-      edited: false,
-      isRead: true,
-      text: "Yeap. You may engage in “nominative use” of the Spring name, but this does not allow you to use the logo.",
-      author: { id: 2, online: true, username: "bot", name: "Bot", lastSeen: 0, createdAt: "", avatarPath: "" },
-    },
-    {
-      id: "4",
-      action: "message",
-      reactions: [],
-      createdAt: "124151234125124",
-      edited: false,
-      isRead: true,
-      text: "You may use the Spring Marks in connection with development of tools, add-ons, etc.",
-      author: { id: 1, online: true, username: "bot", name: "Bot", lastSeen: 0, createdAt: "", avatarPath: "" },
-    },
-    {
-      id: "5",
-      action: "message",
-      reactions: [],
-      createdAt: "124151234125124",
-      edited: false,
-      isRead: true,
-      text: "You may use the Spring Marks in connection with development of tools, add-ons, etc.",
-      author: { id: 1, online: true, username: "bot", name: "Bot", lastSeen: 0, createdAt: "", avatarPath: "" },
-    },
-    {
-      id: "6",
-      action: "message",
-      reactions: [],
-      createdAt: "124151234125124",
-      edited: false,
-      isRead: true,
-      text: "You may use the Spring Marks in connection with development of tools, add-ons, etc.",
-      author: { id: 1, online: true, username: "bot", name: "Bot", lastSeen: 0, createdAt: "", avatarPath: "" },
-    },
-    {
-      id: "7",
-      action: "message",
-      reactions: [],
-      createdAt: "124151234125124",
-      edited: false,
-      isRead: true,
-      text: "You may use the Spring Marks in connection with development of tools, add-ons, etc.",
-      author: { id: 1, online: true, username: "bot", name: "Bot", lastSeen: 0, createdAt: "", avatarPath: "" },
-    },
-    {
-      id: "8",
-      action: "message",
-      reactions: [],
-      createdAt: "124151234125124",
-      edited: false,
-      isRead: true,
-      text: "You may use the Spring Marks in connection with development of tools, add-ons, etc.",
-      author: { id: 1, online: true, username: "bot", name: "Bot", lastSeen: 0, createdAt: "", avatarPath: "" },
-    },
-    {
-      id: "9",
-      action: "message",
-      reactions: [],
-      createdAt: "124151234125124",
-      edited: false,
-      isRead: true,
-      text: "You may use the Spring Marks in connection with development of tools, add-ons, etc.",
-      author: { id: 1, online: true, username: "bot", name: "Bot", lastSeen: 0, createdAt: "", avatarPath: "" },
-    },
-    {
-      id: "10",
-      action: "message",
-      reactions: [],
-      createdAt: "124151234125124",
-      edited: false,
-      isRead: true,
-      text: "You may use the Spring Marks in connection with development of tools, add-ons, etc.",
-      author: { id: 1, online: true, username: "bot", name: "Bot", lastSeen: 0, createdAt: "", avatarPath: "" },
-    },
-    {
-      id: "11",
-      action: "message",
-      reactions: [],
-      createdAt: "124151234125124",
-      edited: false,
-      isRead: true,
-      text: "You may use the Spring Marks in connection with development of tools, add-ons, etc.",
-      author: { id: 1, online: true, username: "bot", name: "Bot", lastSeen: 0, createdAt: "", avatarPath: "" },
-    },
-    {
-      id: "12",
-      action: "message",
-      reactions: [],
-      createdAt: "124151234125124",
-      edited: false,
-      isRead: true,
-      text: "You may use the Spring Marks in connection with development of tools, add-ons, etc.",
-      author: { id: 1, online: true, username: "bot", name: "Bot", lastSeen: 0, createdAt: "", avatarPath: "" },
-    },
-    {
-      id: "13",
-      action: "message",
-      reactions: [],
-      createdAt: "124151234125124",
-      edited: false,
-      isRead: true,
-      text: "You may use the Spring Marks in connection with development of tools, add-ons, etc.",
-      author: { id: 1, online: true, username: "bot", name: "Bot", lastSeen: 0, createdAt: "", avatarPath: "" },
-    },
-    {
-      id: "14",
-      action: "message",
-      reactions: [],
-      createdAt: "124151234125124",
-      edited: false,
-      isRead: true,
-      text: "You may use the Spring Marks in connection with development of tools, add-ons, etc.",
-      author: { id: 1, online: true, username: "bot", name: "Bot", lastSeen: 0, createdAt: "", avatarPath: "" },
-    },
-    {
-      id: "15",
-      action: "message",
-      reactions: [],
-      createdAt: "124151234125124",
-      edited: false,
-      isRead: true,
-      text: "You may use the Spring Marks in connection with development of tools, add-ons, etc.",
-      author: { id: 1, online: true, username: "bot", name: "Bot", lastSeen: 0, createdAt: "", avatarPath: "" },
-    },
-  ];
+export function ChatBox({ contact, messages, onSendMessageClick, onChatHeaderClick }: Readonly<ChatBoxProps>) {
+  const messageContainerRef = React.useRef<HTMLDivElement>(null);
+  const textareaRef = useRef<HTMLTextAreaElement>(null);
+
+  const [initialTextAreaHeight, setInitialTextAreaHeight] = useState<number | undefined>(undefined);
+  const [initialMessageContainerHeight, setInitialMessageContainerHeight] = useState<number | undefined>(undefined);
+  const [showScrollDownButton, setShowScrollDownButton] = useState(false);
+  const [message, setMessage] = useState("");
+
+  const { currentUser } = useAppSelector(state => state.user);
+
+  const scrollToBottom = (behavior?: ScrollBehavior) => {
+    if (messageContainerRef.current) {
+      messageContainerRef.current.scrollTo({
+        top: messageContainerRef.current.scrollHeight,
+        behavior,
+      });
+    }
+  };
+
+  const handleHeaderClick = (headerContact: ContactParams) => {
+    if (onChatHeaderClick) {
+      onChatHeaderClick(headerContact);
+    }
+  };
+
+  const handleSendMessageClick = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    if (message && onSendMessageClick && currentUser) {
+      if (message.trim().length === 0) return;
+      onSendMessageClick(currentUser, message.trim());
+      setMessage("");
+      if (textareaRef.current) {
+        textareaRef.current.style.height = MESSAGE_INPUT.DEFAULT_TEXTAREA_HEIGHT;
+      }
+      scrollToBottom();
+    }
+  };
+
+  const handleTextareaKeyDown = (event: React.KeyboardEvent<HTMLTextAreaElement>) => {
+    if (event.key === "Enter" && !event.shiftKey) {
+      event.preventDefault();
+      handleSendMessageClick(event as unknown as React.FormEvent<HTMLFormElement>);
+    }
+  };
+
+  const handleTextAreaChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => setMessage(event.target.value);
+
+  useLayoutEffect(() => {
+    const handleScroll = () => {
+      if (messageContainerRef.current) {
+        const { scrollTop } = messageContainerRef.current;
+        if (scrollTop < MESSAGE_CONTAINER.SHOW_SCROLL_DOWN_BUTTON_THRESHOLD_HEIGHT) {
+          setShowScrollDownButton(true);
+        } else {
+          setShowScrollDownButton(false);
+        }
+      }
+    };
+
+    if (messageContainerRef.current) {
+      messageContainerRef.current.addEventListener("scroll", handleScroll);
+    }
+
+    return () => {
+      if (messageContainerRef.current) {
+        messageContainerRef.current.removeEventListener("scroll", handleScroll);
+      }
+    };
+  }, []);
+
+  const handleScrollToBottomButtonClick = () => {
+    scrollToBottom("smooth");
+  };
+
+  useLayoutEffect(() => {
+    if (textareaRef.current && messageContainerRef.current) {
+      let initTextAreaHeight: number = initialTextAreaHeight as number;
+      let initMessageContainerHeight: number = initialMessageContainerHeight as number;
+
+      if (!initialTextAreaHeight || !initialMessageContainerHeight) {
+        initTextAreaHeight = textareaRef.current.scrollHeight;
+        initMessageContainerHeight = messageContainerRef.current.offsetHeight;
+
+        setInitialMessageContainerHeight(initMessageContainerHeight);
+        setInitialTextAreaHeight(initTextAreaHeight);
+      }
+
+      textareaRef.current.style.height = `${initTextAreaHeight}px`;
+      textareaRef.current.style.height =
+        textareaRef.current.scrollHeight < MESSAGE_INPUT.MAX_TEXTAREA_HEIGHT
+          ? `${textareaRef.current.scrollHeight}px`
+          : `${MESSAGE_INPUT.MAX_TEXTAREA_HEIGHT}px`;
+
+      const currentTextAreaHeight = textareaRef.current.offsetHeight;
+      const newMessageContainerHeight = initMessageContainerHeight - currentTextAreaHeight + initTextAreaHeight;
+
+      messageContainerRef.current.style.height = `${initMessageContainerHeight}px`;
+      messageContainerRef.current.style.height = `${newMessageContainerHeight}px`;
+    }
+  }, [initialMessageContainerHeight, initialTextAreaHeight, message]);
+
   return (
     <div className="relative h-full w-full flex flex-col justify-between">
-      <ChatHeader user={contact.user} />
-      <MessageContainer messages={messages} />
-      <MessageInput />
+      <div className="flex flex-col w-full h-full">
+        <ChatHeader contact={contact} onChatHeaderClick={handleHeaderClick} />
+        <MessageContainer
+          ref={messageContainerRef}
+          messages={messages}
+          onScrollToBottomButtonClick={handleScrollToBottomButtonClick}
+          showScrollDownButton={showScrollDownButton}
+        />
+        <MessageInput
+          ref={textareaRef}
+          onTextAreaChange={handleTextAreaChange}
+          onTextAreaKeyDown={handleTextareaKeyDown}
+          onSendMessageClick={handleSendMessageClick}
+          message={message}
+        />
+      </div>
     </div>
   );
 }
--- frontend/src/components/Chat/MainBox/variants/ChatBox/MessageBox/Message/Message.tsx
+++ frontend/src/components/Chat/MainBox/variants/ChatBox/MessageBox/Message/Message.tsx
@@ -2,45 +2,64 @@ import { format } from "date-fns";
 
 import { MessageParams } from "@/api/http/contacts/contacts.types";
 import { DoubleCheckIcon } from "@/components/DoubleCheckIcon/DoubleCheckIcon";
+import { Icon } from "@/components/Icon/Icon";
 import { COLORS } from "@/constants/colors";
+import { formatDate } from "@/helpers/FormatDate/formatDate";
+import { DateFormat } from "@/helpers/FormatDate/formatDate.types";
 import { useAppSelector } from "@/lib/hooks";
 
 export type MessageProps = {
   message: MessageParams;
-  previousMessage?: MessageParams;
+  nextMessage?: MessageParams;
 };
 
-export function Message({ message, previousMessage }: Readonly<MessageProps>) {
+export function Message({ message, nextMessage }: Readonly<MessageProps>) {
   const { currentUser } = useAppSelector(state => state.user);
-  const messageCreatedAt = format(Date.now(), "H:mm");
+  const messageCreatedAt = format(message.createdAt * 1000, "H:mm");
+  const date = new Date(message.createdAt * 1000);
+  const nextDate = nextMessage && new Date(nextMessage.createdAt * 1000);
   const isCurrentUserAuthor = message.author.id === currentUser?.id;
-  const isPreviousMessageAuthor = previousMessage?.author.id === message.author?.id;
+  const isPreviousMessageAuthor = nextMessage?.author.id === message.author.id;
+  const isFirstMessageFromUser = !isPreviousMessageAuthor;
+  const shouldShowDate = !nextDate || nextDate.getDate() !== date.getDate();
+
   let messageContainer: string;
   let messageCloudStyle: string;
 
   if (isCurrentUserAuthor) {
     messageContainer = "self-end items-end";
-    messageCloudStyle = `bg-dark-100 ${isPreviousMessageAuthor ? "rounded-tr-2xl" : "rounded-tr-none"}`;
+    messageCloudStyle = isFirstMessageFromUser ? "bg-dark-100 rounded-2xl rounded-tr-none" : "bg-dark-100 rounded-2xl";
   } else {
     messageContainer = "self-start items-start";
-    messageCloudStyle = `bg-dark-200 ${isPreviousMessageAuthor ? "rounded-tl-2xl" : "rounded-tl-none"}`;
+    messageCloudStyle = isFirstMessageFromUser ? "bg-dark-200 rounded-2xl rounded-tl-none" : "bg-dark-200 rounded-2xl";
   }
 
-  if (message.author.id === previousMessage?.author.id) {
-    messageCloudStyle += " rounded-tr-2xl rounded-tl-2xl";
-  }
   return (
-    <div className={`w-3/5 flex flex-col relative -z-10 ${messageContainer}`}>
-      <div className={`text-gray-200 px-6 py-2 rounded-2xl ${messageCloudStyle}`}>{message.text}</div>
-      <div className="flex gap-2">
-        <span className="text-sm text-gray-400">{messageCreatedAt}</span>
-        {isCurrentUserAuthor && (
-          <DoubleCheckIcon
-            iconSize={15}
-            checkIconColor={COLORS.blue["200"]}
-            cutCheckIconColor={message.isRead ? COLORS.blue["200"] : COLORS.dark["150"]}
-          />
-        )}
+    <div className="w-full h-max relative flex flex-col">
+      {shouldShowDate && (
+        <span key={date.getTime()} className="self-end flex gap-2 text-gray-500 font-semibold">
+          <Icon name="clock-outline" iconSize={18} />
+          {formatDate(date.getTime(), DateFormat.MONTH_AND_DAY)}
+          <Icon name="line-horizontal" iconSize={49} />
+        </span>
+      )}
+      <div className={`w-3/5 flex flex-col -z-10 ${messageContainer}`}>
+        <div className={`text-gray-200 px-6 py-2 whitespace-pre-wrap break-all ${messageCloudStyle}`}>
+          {message.text}
+        </div>
+        <div className="flex gap-2">
+          <span className="text-sm text-gray-400">{messageCreatedAt}</span>
+          {isCurrentUserAuthor &&
+            (message.sending ? (
+              <Icon name="clock-outline" iconSize={15} color={COLORS.gray["500"]} />
+            ) : (
+              <DoubleCheckIcon
+                iconSize={15}
+                checkIconColor={COLORS.blue["200"]}
+                cutCheckIconColor={message.isRead ? COLORS.blue["200"] : COLORS.dark["150"]}
+              />
+            ))}
+        </div>
       </div>
     </div>
   );
