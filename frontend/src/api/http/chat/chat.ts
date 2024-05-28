b4c68b7c271969bc13d3b56f807934a5104f8d04
Artem Magei <artemmagej@gmail.com>
2024-05-28 12:16:24 +0300

    feat: update api

--- frontend/src/api/http/chat/chat.ts
+++ frontend/src/api/http/chat/chat.ts
@@ -1,5 +1,12 @@
 import { instance } from "@/api/http";
-import { ChatParams, MessageParams } from "@/api/http/contacts/contacts.types";
+import {
+  ChatParams,
+  FileHttpResponse,
+  GroupChatDetails,
+  GroupMember,
+  MessageParams,
+} from "@/api/http/contacts/contacts.types";
+import { ChatType } from "@/api/socket/index.types";
 
 export type AddDuoChatParams = {
   id: string;
@@ -23,8 +30,16 @@ export async function getChats(offset: number = 0, limit: number = 10) {
   const { data } = await instance.get<ChatParams[]>(`chats?limit=${limit}&offset=${offset}`);
   const chats = new Map<string, ChatParams>();
   data.forEach(chat => {
-    chats.set(chat.id, chat);
+    if (chat.type === ChatType.GROUP) {
+      chats.set(chat.id, chat);
+    } else {
+      chats.set(chat.id, {
+        ...chat,
+        name: chat.user.name,
+      });
+    }
   });
+  console.log(chats);
   return chats;
 }
 
@@ -34,5 +49,45 @@ export async function getMessagesByChatId(chatId: string, offset: number = 0, li
   data.forEach(chat => {
     messages.set(chat.id, chat);
   });
+  console.log(data);
   return messages;
+}
+
+export async function createGroupChat(name: string, description: string, isPrivate: boolean) {
+  const body = {
+    name,
+    description,
+    isPrivate,
+  };
+  const { data } = await instance.post<ChatParams>(`chats/group`, body);
+  console.log(data);
+  return data;
+}
+
+export async function getGroupChatDetailsById(chatId: string) {
+  const { data } = await instance.get<GroupChatDetails>(`chats/${chatId}/group`);
+  const members = new Map<string, GroupMember>();
+  data.members.forEach(member => {
+    members.set(member.id, member);
+  });
+  return { ...data, members };
+}
+
+export async function sendFile(chatId: string, file: File) {
+  const formData = new FormData();
+  formData.append("file", file);
+
+  const config = {
+    headers: {
+      "Content-Type": "multipart/form-data",
+    },
+  };
+
+  const { data } = await instance.post<FileHttpResponse>(`chats/${chatId}/messages/file`, formData, config);
+  return data;
+}
+
+export async function getFile(messageId: string) {
+  const { data } = await instance.get(`chats/messages/${messageId}/file`, { responseType: "blob" });
+  return data;
 }
--- frontend/src/api/http/index.ts
+++ frontend/src/api/http/index.ts
@@ -6,7 +6,7 @@ import { isTokenExpired } from "@/helpers/DecodeToken/decodeToken";
 import { store } from "@/lib/store";
 
 export const LIST_PAGINATION_LIMIT = 20;
-const baseURL = process.env.NEXT_PUBLIC_API_URL ?? "http://localhost:8080/";
+export const baseURL = process.env.NEXT_PUBLIC_API_URL ?? "http://localhost:8080/";
 
 export const instance: AxiosInstance = axios.create({
   baseURL,
